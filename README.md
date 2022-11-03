1e2d382260d9dea851e7aa5a717108e932f2f8b9
JJunny0109 <jjunny0109@gmail.com>
2022-11-04 00:06:56 +0900

    Add Key_Number Feature

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# beCarefulForAssasinators
-To Encrypt or Decrypt the Caesar Cipher, Use my program.
+# 카이사르 암호란? (Caesar Cipher)
+카이사르 암호는 각각의 알파벳을 일정한 거리만큼 밀어 글자를 치환하는 방식으로 암호화한다. 위 예제에서는 3글자씩 밀어서 암호화하기 때문에 B는 E로 치환된다.
+
+![안녕](https://upload.wikimedia.org/wikipedia/commons/2/2b/Caesar3.svg)
+
+# 업데이트 내역 (v0.1.1)
+    - 이제 암호키를 설정할 수 있게 되었습니다.
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let plainText = document.querySelector("#plainTextArea").value; //입력한 메시지
 const resultText = document.querySelector("#resultTextArea"); //출력할 장소
-let KEY_NUMBER = 3; //키코드 언제든지 변경가능하여야함
+let KEY_NUMBER = Number(document.querySelector("#keyNumber").value); //키코드 언제든지 변경가능하여야함
 const aryEnglish = /[a-zA-Z]/; //영어인지 구별하는 배열
 
 
@@ -8,7 +8,6 @@ const aryEnglish = /[a-zA-Z]/; //영어인지 구별하는 배열
 function encryptMessage(){
   let encodedCipher = ""
   for (var i = 0; i < plainText.length; i++) {
-    
     // plainMessage에서 i번째 글자가 알파뱃인지를 구별
     if ((aryEnglish.test(plainText.charAt(i)))){
       // 키코드에서 1을 제하고, 나머지에 1을 더해야 0이 나오지 않는다. A = 1, B = 2 라서 이렇게 구성하였음
@@ -27,6 +26,7 @@ function encryptMessage(){
 //키가 입력될 때 writeOnIt 함수 실행
 function writeOnIt(){
   plainText = document.querySelector("#plainTextArea").value;
+  KEY_NUMBER = Number(document.querySelector("#keyNumber").value);
   resultText.innerText = encryptMessage();
 }
 document.addEventListener("keyup",writeOnIt);
